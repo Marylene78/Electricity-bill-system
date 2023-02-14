92cce92bac1f3ca901d64d1660b1a245f04338b6
Marylene78 <123755478+Marylene78@users.noreply.github.com>
2023-02-14 20:34:24 +0300

    Add files via upload

---
+++ electricity bill system/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{   int bills,units;
+    printf("Enter units consumed\n");
+    scanf("%d",&units);
+     bills=20*units;
+    printf("%d",bills);
+
+    return 0;
+}
